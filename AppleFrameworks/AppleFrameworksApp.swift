6dfa6a4607ac8e451964986864426eb5e2884c15
w77sh <iraqxgmar915@gmail.com>
2025-07-04 04:38:18 +0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by AbdulRahman Habeeb on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFrameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleFrameworks
+//
+//  Created by AbdulRahman Habeeb on 04/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
